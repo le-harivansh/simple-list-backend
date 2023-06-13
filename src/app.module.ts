c13797490c277af17a8a564749eefb783698495d
Harivansh <hello@harivan.sh>
2023-06-13 12:55:07 +0400

    feat: add list module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ListModule } from './list/list.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ListModule],
 })
 export class AppModule {}
