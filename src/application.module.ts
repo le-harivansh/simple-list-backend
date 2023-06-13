aa3cdf94f296bde18986b9a7792b9c0cfcb16e16
Harivansh <hello@harivan.sh>
2023-06-13 18:56:02 +0400

    feat: configure database

--- src/application.module.ts
+++ src/application.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ListModule } from './list/list.module';
+import { ItemModule } from './item/item.module';
 import { ConfigModule } from '@nestjs/config';
 import applicationConfig from './application.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import path from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     ConfigModule.forFeature(applicationConfig),
-    ListModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: `${path.resolve(__dirname, '..')}/database/database.sqlite`,
+      autoLoadEntities: true,
+      logging: true,
+    }),
+    ItemModule,
   ],
 })
 export class ApplicationModule {}
