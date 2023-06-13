90ac27fcb07a3d9c93ea2dcb73390042fcdef58e
Harivansh <hello@harivan.sh>
2023-06-13 13:15:58 +0400

    refactor: change application-port env variable

--- src/application.config.ts
+++ src/application.config.ts
@@ -3,5 +3,5 @@ import { registerAs } from "@nestjs/config";
 const DEFAULT_APPLICATION_PORT = 3000;
 
 export default registerAs('application', () => ({
-    port: process.env.PORT || DEFAULT_APPLICATION_PORT,
+    port: process.env.APPLICATION_PORT || DEFAULT_APPLICATION_PORT,
 }));
