5701ed43570f6dca9b646c748e01c176b19505f9
Harivansh <hello@harivan.sh>
2023-06-13 18:56:17 +0400

    refactor: remove unnecessary files

--- src/list/list.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class ListController {
-    @Get('')
-    public index() {
-        return ['item 1', 'item 2'];
-    }
-}
--- src/list/list.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ListController } from './list.controller';
-
-@Module({
-  controllers: [ListController]
-})
-export class ListModule {}
