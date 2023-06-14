32d294c731457c9ee7629c038259af37fb2d6ae9
Harivansh <hello@harivan.sh>
2023-06-14 09:58:53 +0400

    feat: first working version of migration running

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,11 +1,15 @@
 import path from "path";
 import { DataSource, DataSourceOptions } from "typeorm";
 import { Item } from "./item/item.entity";
+import { CreateItemsTable1686721941399 } from "./migrations/1686721941399-create_items_table";
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'sqlite',
     database: `${path.resolve(__dirname, '..')}/database/database.sqlite`,
     entities: [Item],
+    migrations: [
+        CreateItemsTable1686721941399
+    ]
 }
 
 const dataSource = new DataSource(dataSourceOptions);
--- src/migrations/1686721941399-create_items_table.ts
+++ src/migrations/1686721941399-create_items_table.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateItemsTable1686721278192 implements MigrationInterface {
-    name = 'CreateItemsTable1686721278192'
+export class CreateItemsTable1686721941399 implements MigrationInterface {
+    name = 'CreateItemsTable1686721941399'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "items" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "title" varchar NOT NULL)`);
